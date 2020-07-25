1dcca1b87300a08ba7d9ab9a523dc5d280da0135
Seth <colts18seth@gmail.com>
2020-07-25 09:45:27 -0400

    starting get route query string args

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 /** Express app for jobly. */
 
 const express = require("express");
-
 const ExpressError = require("./helpers/expressError");
-
 const morgan = require("morgan");
-
 const app = express();
 
 app.use(express.json());
@@ -14,24 +11,22 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 /** 404 handler */
+app.use(function (req, res, next) {
+    const err = new ExpressError("Not Found", 404);
 
-app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-
-  // pass the error to the next piece of middleware
-  return next(err);
+    // pass the error to the next piece of middleware
+    return next(err);
 });
 
 /** general error handler */
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    console.error(err.stack);
 
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  console.error(err.stack);
-
-  return res.json({
-    status: err.status,
-    message: err.message
-  });
+    return res.json({
+        status: err.status,
+        message: err.message
+    });
 });
 
 module.exports = app;
